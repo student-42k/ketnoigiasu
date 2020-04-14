df4c146517e97889ae7bfef3fc81bc00c793a973
mitomxao605 <59536001+mitomxao605@users.noreply.github.com>
2020-04-14 11:22:48 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
+<div class="footer1"><br />
+				TTPHCM: 1073/51 Cách Mạng Tháng 8, Phường 7, Q. Tân Bình, TPHCM<br />
    				 Điện thoại: 0903978794 <br />Bình Dương: 504 Đại lộ Bình Dương, Thủ Dầu Một, Bình Dương<br />
    				 Điện thoại: 0902684422 <br />Đồng Nai: 4 Lê Qúy Đôn, TP. Biên Hòa, Đồng Nai<br />
+    			Điện thoại: 0906974114 <br />Cần Thơ: Khu 2 Đường 3 Tháng 2, Ninh Kiều, Cần Thơ<br />
+    			Điện thoại: 0907971009	<br /><br /></div>
     			
     			<div class="footer2">
     				<h1>BẢN QUYỀN THUỘC VỀ TEAM 42K<br />
+    				MÔN QUẢN TRỊ DỰ ÁN CÔNG NGHỆ THÔNG TIN<br />
+    				TRƯỜNG ĐẠI HỌC KINH TẾ ĐÀ NẴNG</h1>
     			</div>
